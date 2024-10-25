16a98739c448af2eb662e16e722c214ec4271c72
Ting <geniusyang924@gmail.com>
2024-10-25 19:53:21 +0800

    itinerary-detail 頁面大致完成差JS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ItineraryListComponent } from './Product/itinerary-list/itinerary-list.component';
 import { ItineraryDetailComponent } from './Product/itinerary-detail/itinerary-detail.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ItineraryDetailComponent } from './Product/itinerary-detail/itinerary-d
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
