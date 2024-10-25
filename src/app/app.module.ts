41a6a5ceec43d2604bcf06b61fabf4afc3ee201b
jojo <User@DESKTOP-RAE7E6C>
2024-10-25 11:46:10 +0800

    1025

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ItineraryListComponent } from './Product/itinerary-list/itinerary-list.component';
 
 import { TicketsComponent } from './Shipment/tickets/tickets.component';
+import { DetailComponent } from './Shipment/detail/detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ItineraryListComponent,
     
-    TicketsComponent
+    TicketsComponent,
+          DetailComponent
   ],
   imports: [
     BrowserModule,
