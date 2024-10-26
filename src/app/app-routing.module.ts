9835a9d62014fa83b0e647d8069bb3194d555999
Luke <qwe58912@gmail.com>
2024-10-27 02:32:54 +0800

    1.新增付款成功component 2.router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { ItineraryListComponent } from './components/product/itinerary-list/itinerary-list.component';
 import { ItineraryDetailComponent } from './components/product/itinerary-detail/itinerary-detail.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { CartComponent } from './components/Order/cart/cart.component';
+import { CheckoutComponent } from './components/Order/checkout/checkout.component';
+import { OrderConfirmationComponent } from './components/Order/order-confirmation/order-confirmation.component';
 
 const routes: Routes = [
   { path: 'itinerary-list', component: ItineraryListComponent },
   { path: 'itinerary-list/area_:region', component: ItineraryListComponent },
   { path: 'itinerary-detail/:id', component: ItineraryDetailComponent },
   { path: '', component:HomePageComponent },
-  { path: 'home', component:HomePageComponent }
+  { path: 'home', component:HomePageComponent },
+  { path: 'cart', component:CartComponent },
+  { path: 'checkout', component:CheckoutComponent },
+  { path: 'orderconfirmation', component:OrderConfirmationComponent }
+
 ];
 
 @NgModule({
---
+++ src/app/components/Order/cart/cart.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent {
+
+  constructor(private router: Router) {
+
+  }
+
+  goToCheckout() {
+    this.router.navigate(['checkout'])
+  }
+
+}
