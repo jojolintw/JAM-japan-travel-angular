3e55bde760eae00cd92f1785b039b0295cbcb0cf
song <r8976a1670@gmail.com>
2024-10-27 21:41:00 +0800

    BLOG初版

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { ItineraryListComponent } from './components/product/itinerary-list/itinerary-list.component';
 import { ItineraryDetailComponent } from './components/product/itinerary-detail/itinerary-detail.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import{BlogListComponent} from'./components/Blog/blog-list/blog-list.component';
 
 const routes: Routes = [
   { path: 'itinerary-list', component: ItineraryListComponent },
   { path: 'itinerary-list/area_:region', component: ItineraryListComponent },
   { path: 'itinerary-detail/:id', component: ItineraryDetailComponent },
   { path: '', component:HomePageComponent },
-  { path: 'home', component:HomePageComponent }
+  { path: 'home', component:HomePageComponent },
+  { path:'blog-list',component:BlogListComponent}
 ];
 
 @NgModule({
