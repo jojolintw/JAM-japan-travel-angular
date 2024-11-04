aa4cbf39893e8c8f39f087d849bdb17fa4204377
Luke <qwe58912@gmail.com>
2024-11-04 22:55:33 +0800

    1.購物車管理 service 待完成 2.購物車數量 button 待完成

--- src/app/interface/Order/cartItem.ts
+++ src/app/interface/Order/cartItem.ts
@@ -1,9 +1,10 @@
 export interface cartItem{
-  itineraryImage:string
-  itineraryName:string
-  price:number
-  quantity:number
-  totalPrice:number
-  couponName:string
-  discount:number
+  id:number             //商品ID
+  name:string           //商品名稱
+  price:number          //商品單價
+  quantity:number       //數量
+  imagePath:string      //圖片路徑
+  type:string           //區分行程與船票
+  couponName:string     //優惠券名稱
+  discount:number       //折扣
 }
